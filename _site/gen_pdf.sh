a33c486bef2e3fa9bcdc3f2bacf20a3d07cb0545
Sylvain Saubier (ResponSyS) <mail@systemicresponse.com>
2017-11-20 20:07:25 +0100

    gen_pdf.sh + pdf + favicon

--- _site/gen_pdf.sh
+++ _site/gen_pdf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash - 
 #===============================================================================
 #
-#         USAGE: ./gen_pdf.sh 
+#        USAGE: ./gen_pdf.sh 
 # 
-#   DESCRIPTION: Build PDF pages recursively
+#   DESCRIPTION: Build PDF pages from MD recursively
 # 
 #       OPTIONS: ---
-#  REQUIREMENTS: ---
+#  REQUIREMENTS: find, sed, xargs, pandoc
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Sylvain Saubier (ResponSyS), mail@systemicresponse.com
@@ -15,17 +15,25 @@
 #      REVISION:  ---
 #===============================================================================
 
-# TODO: NOT WORKING YET
+# TODO: arg parsing with getopt
+# TODO: --no-interactive option to deactive xargs '-p' flag
+# TODO: customize the template
+# `toc: true` in YAML header of MD: does it work?
 
 set -o nounset -o errexit
 
-PROGRAM_NAME="gen.sh"
-BASEDIR="$HOME/Devel/Src/Web/responsyss.github.io/"
+PROGRAM_NAME="gen_pdf.sh"
+#BASEDIR="$HOME/Devel/Src/Web/responsyss.github.io/"
+BASEDIR="."
 
-# templates
-F_TEMPL="texte.pdf.pandoc"
-# special
-F_TEXTES_DIR="textes"
+F_TEMPL="template.pdf.pandoc"
+F_TEXTES_DIR="${BASEDIR}/textes"
+SEARCH_PATH=""
+
+XARGS_FLAGS="-P4 -p"
+# is log erased each time? if yes it's retarded to set it
+#PANDOC_FLAGS="-f markdown --template ${BASEDIR}/${F_TEMPL} --fail-if-warnings --log=$PROGRAM_NAME.log"
+PANDOC_FLAGS="-f markdown --template ${BASEDIR}/${F_TEMPL} --fail-if-warnings"
 
 ERR_NO_CMD=127
 ERR_WRONG_WORKING_DIR=101
@@ -33,33 +41,33 @@ ERR_NO_FILE=40
 
 # $1 = message (string)
 fn_say() {
-    echo "$PROGRAM_NAME: $1"
+	echo "$PROGRAM_NAME: $1"
 }
 # $1 = error message (string), $2 = return code (int)
 fn_err() {
-    fn_say "$1" >&2
-    exit $2
+	fn_say "$1" >&2
+	exit $2
 }
 # $1 = command to test (string)
 fn_needCmd() {
-    if ! command -v "$1" > /dev/null 2>&1
-    then fn_err "need '$1' (command not found)" $ERR_NO_CMD
-    fi
+	if ! command -v "$1" > /dev/null 2>&1
+		then fn_err "need '$1' (command not found)" $ERR_NO_CMD
+	fi
 }
 # $1 = file to test (string)
 fn_needFile() {
-    if ! test -f "$1" > /dev/null 2>&1
-    then fn_err "need '$1' (file not found)" $ERR_NO_FILE
-    fi
+	if ! test -e "$1" > /dev/null 2>&1
+		then fn_err "need '$1' (file or directory not found)" $ERR_NO_FILE
+	fi
 }
 
 fn_help() {
-    cat 1>&2 << EOF
+	cat 1>&2 << EOF
 USAGE
-    ./$PROGRAM_NAME [textes|PATH]
+    ./$PROGRAM_NAME [all|PATH]
 
 OPTIONS
-    textes      recursively convert all MD files to PDF in /textes/ (default)
+    all         recursively convert all MD files to PDF in ${BASEDIR}/${F_TEXTES_DIR}/ (default)
     {PATH}      recursively convert all MD files to PDF in PATH
 
 EXAMPLE
@@ -74,14 +82,25 @@ REPORTING BUGS
 EOF
 }
 
+# « xargs c'est la vie » -- Mt, 12:24
+# « Le gras c'est la vie » -- Karadoc, Kaamelott 
 fn_genTextes() {
-    find $SEARCH_PATH -name "*.pdc" -execdir echo \
-        pandoc -t pdf --template ${BASEDIR}/${F_TEMPL} {} -o {}.pdf \;
-    find $SEARCH_PATH -name "*.pdc" -execdir \
-        pandoc -t pdf --template ${BASEDIR}/${F_TEMPL} {} -o {}.pdf \;
+	find $SEARCH_PATH -type f -name "*.md" | sort | uniq | sed 's/[.]md$//' | xargs -I'{}' $XARGS_FLAGS pandoc $PANDOC_FLAGS   {}.md -o {}.pdf
+}
+
+fn_printParams() {
+	cat 1>&2 << EOF
+F_TEMPL        $F_TEMPL
+F_TEXTES_DIR   $F_TEXTES_DIR
+SEARCH_PATH    $SEARCH_PATH
+XARGS_FLAGS    $XARGS_FLAGS
+PANDOC_FLAGS   $PANDOC_FLAGS
+EOF
 }
 
 fn_needCmd "find"
+fn_needCmd "sed"
+fn_needCmd "xargs"
 fn_needCmd "pandoc"
 fn_needFile "$F_TEMPL"
 fn_needFile "$F_TEXTES_DIR"
@@ -93,29 +112,33 @@ fn_say "current directory: `pwd`"
 # USE GETOPT instead
 # Arguments parsing
 if test $# -eq 0; then
-    fn_help
-    exit
+	fn_help
+	exit
 fi
 while test $# -ge 1; do
-    case "$1" in
-        "textes")
-		# adds "textes/" to SEARCH_PATH
-		SEARCH_PATH="$SEARCH_PATH $F_TEXTES_DIR"
-            ;;
-        *)
-		# adds arg to search pathes
-            ;;
-    esac	# --- end of case ---
-    # Delete $1
-    shift
+	case "$1" in
+		"all")
+			# adds "textes/" to SEARCH_PATH
+			SEARCH_PATH="$SEARCH_PATH $F_TEXTES_DIR"
+			;;
+		"-h"|"--help")
+			fn_help
+			exit
+			;;
+		*)
+			# adds arg to search pathes
+			SEARCH_PATH="$SEARCH_PATH $1"
+			;;
+	esac	# --- end of case ---
+	# Delete $1
+	shift
 done
 
-# if all files in SEARCH_PATH are existing files
-# for f in $SEARCH_PATH ; do
-# 	if not test -f $f
-# 		fn_help
-# 		exit
-# done
+for FILE in $SEARCH_PATH ; do
+	fn_needFile "$FILE"
+done
+
+fn_printParams
 
 fn_say "generating..."
 fn_genTextes
