0060d20c5d234f83ad43121e0102eb6529893f91
Sylvain Saubier (ResponSyS) <mail@systemicresponse.com>
2017-12-18 18:57:51 +0100

    Improved helper script for ./pdc_gen.sh

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -
 ## NOTES ##
 # https://github.com/ResponSySS/responsyss.github.io/wiki/make_pdf.sh
+SEARCH_PATH="textes/" 
+CMD=(./pdc_gen.sh -i md -o pdf:epub -a '--template=./template.pdf.pandoc' -j 1 -p)
 
-./pdc_gen.sh textes/ -i md -o pdf:epub -a '--template=./template.pdf.pandoc' -j 1 -p
+case "$1" in 	"help"|"usage"|"-h"|"--help")
+			{ echo -e "[$0] Usage: $0 [SEARCH_PATH]\t(SEARCH_PATH defaults to: \"${SEARCH_PATH}\"; see ${CMD[0]} --help)\n[$0] Command to be executed: ${CMD[@]} [SEARCH_PATH]"; exit; }
+			;;
+		*)
+			SEARCH_PATH="$1"
+			;;
+esac
 
+${CMD[@]} ${SEARCH_PATH}
