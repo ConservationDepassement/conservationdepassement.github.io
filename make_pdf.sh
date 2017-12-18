5fd7315c7b6ad1fa55732c85918fefe7d0e0b916
Sylvain Saubier (ResponSyS) <mail@systemicresponse.com>
2017-12-18 19:25:35 +0100

    styling

--- make_pdf.sh
+++ make_pdf.sh
@@ -5,11 +5,9 @@ SEARCH_PATH="textes/"
 CMD=(./pdc_gen.sh -i md -o pdf:epub -a '--template=./template.pdf.pandoc' -j 1 -p)
 
 case "$1" in 	"help"|"usage"|"-h"|"--help")
-			{ echo -e "[$0] Usage: $0 [SEARCH_PATH]\t(SEARCH_PATH defaults to: \"${SEARCH_PATH}\"; see ${CMD[0]} --help)\n[$0] Command to be executed: ${CMD[@]} [SEARCH_PATH]"; exit; }
-			;;
+			{ echo -e "[$0] Usage: $0 [SEARCH_PATH]\t(SEARCH_PATH defaults to: \"${SEARCH_PATH}\"; see ${CMD[0]} --help)\n[$0] Command to be executed: ${CMD[@]} [SEARCH_PATH]"; exit; } ;;
 		*)
-			SEARCH_PATH="$1"
-			;;
+			SEARCH_PATH="$1" ;;
 esac
 
 ${CMD[@]} ${SEARCH_PATH}
